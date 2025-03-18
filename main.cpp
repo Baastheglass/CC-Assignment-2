264e8d98cb197f367c0008a5e3489c23c839932c
Baastheglass <lightningblazer123@gmail.com>
2025-03-18 13:03:18 +0500

    fixed ptr

--- main.cpp
+++ main.cpp
@@ -37,7 +37,8 @@ int main() {
     Value *divide = builder.CreateSDiv(arg1, arg2);
 
     //initialising print Functionality
-    std::vector<Type *> printfParams = {builder.getInt8Ty()};
+    module->setDataLayout("e-m:e-i64:64-f80:128-n8:16:32:64-S128");
+    std::vector<Type *> printfParams = {builder.getPtrTy()};
     FunctionType *printfTy = FunctionType::get(builder.getInt32Ty(), printfParams, true);
     Function *printfFunc = Function::Create(printfTy, Function::ExternalLinkage, "printf", *module);
 
